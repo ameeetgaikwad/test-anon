7c10d533d061aa6812e1d074aef6ff64be3cbda2
Amit Gaikwad <amitmanojgaikwad@gmail.com>
2024-03-29 16:33:18 +0530

    chore: chore

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { AnonAadhaarProvider } from "@anon-aadhaar/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AnonAadhaarProvider _useTestAadhaar={true}>
+      <Component {...pageProps} />
+    </AnonAadhaarProvider>
+  );
 }
